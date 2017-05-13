fbff2728b4891183eb97446eb5a3526419477b64
Daniel Corn <info@cundd.net>
2017-05-13 17:51:38 +0200

    Added modification and iteration support for the matrix
    Made struct members private
    Added getter data()
    Added getter rows()
    Added getter columns()
    Added push_row()
    Added prepend_row()
    Added implementation for IntoIterator
    Removed implementation for from_vec_ref()

--- src/formatter/console_formatter/matrix.rs
+++ src/formatter/console_formatter/matrix.rs
@@ -1,10 +1,12 @@
 use std::fmt::Debug;
+use std::iter::IntoIterator;
+use std::vec::IntoIter as VecIntoIter;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Matrix<T> where T: Clone {
-    pub rows: usize,
-    pub columns: usize,
-    pub data: Vec<Vec<T>>
+    rows: usize,
+    columns: usize,
+    data: Vec<Vec<T>>
 }
 
 impl<T> Matrix<T> where T: Clone + Debug {
@@ -29,13 +31,30 @@ impl<T> Matrix<T> where T: Clone + Debug {
         }
     }
 
+    #[allow(unused)]
     pub fn from_vec_ref(input: &Vec<Vec<T>>) -> Matrix<T> {
-        Matrix::from_vec(input.to_owned())
+        unimplemented!()
     }
 
     pub fn from_slice(input: &[&[T]]) -> Matrix<T> {
         Matrix::from_vec(input.iter().map(|x| x.to_vec()).collect())
     }
+
+    pub fn data(&self) -> &Vec<Vec<T>> {
+        &self.data
+    }
+
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
+
+    pub fn columns(&self) -> usize {
+        self.columns
+    }
+
+    //    pub fn iter(&self) -> Iter<T> {
+    //        self.data.into_iter()
+    //    }
 
     pub fn is_empty(&self) -> bool {
         self.rows == 0 && self.columns == 0
@@ -66,6 +85,27 @@ impl<T> Matrix<T> where T: Clone + Debug {
             data: new_data,
         }
     }
+
+    pub fn push_row(&mut self, row: Vec<T>) {
+        self.data.push(row);
+        self.rows += 1;
+    }
+
+    pub fn prepend_row(&mut self, row: Vec<T>) {
+        self.data.insert(0, row);
+        self.rows += 1;
+    }
+}
+
+
+impl<T: Clone> IntoIterator for Matrix<T> {
+    type Item = Vec<T>;
+
+    type IntoIter = VecIntoIter<Vec<T>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
+    }
 }
 
 
@@ -92,12 +132,94 @@ mod tests {
     }
 
     #[test]
-    fn from_vec_ref_test() {
-        let m = Matrix::from_vec_ref(&build_data());
+    fn from_slice_test() {
+        let apple = vec!("Apple", "Red");
+        let pear = vec!("Pear", "Green");
+        let banana = vec!("Banana", "Yellow");
+        let orange = vec!("Orange", "Orange");
+
+        let data = vec!(
+            apple.as_slice(),
+            pear.as_slice(),
+            banana.as_slice(),
+            orange.as_slice(),
+            );
+        let m = Matrix::from_slice(&data);
 
         assert_eq!(4, m.rows);
         assert_eq!(2, m.columns);
         assert_eq!(build_data(), m.data);
+    }
+
+    //    #[test]
+    //    fn from_vec_ref_test() {
+    //        let m = Matrix::from_vec_ref(&build_data());
+    //
+    //        assert_eq!(4, m.rows);
+    //        assert_eq!(2, m.columns);
+    //        assert_eq!(build_data(), m.data);
+    //    }
+
+    #[test]
+    fn data_test() {
+        let m = Matrix::from_vec(build_data());
+
+        assert_eq!(
+        vec!(
+            vec!("Apple", "Red"),
+            vec!("Pear", "Green"),
+            vec!("Banana", "Yellow"),
+            vec!("Orange", "Orange"),
+            ),
+        m.data
+        );
+    }
+
+    #[test]
+    fn rows_test() {
+        let m = Matrix::from_vec(build_data());
+
+        assert_eq!(4, m.rows);
+    }
+
+    #[test]
+    fn columns_test() {
+        let m = Matrix::from_vec(build_data());
+
+        assert_eq!(2, m.columns);
+    }
+
+    #[test]
+    fn into_iter_test() {
+        let m = Matrix::from_vec(build_data());
+
+        let iterator = m.into_iter();
+        for (i, row) in iterator.enumerate() {
+            match i {
+                0 => assert_eq!(vec!("Apple", "Red"), row),
+                1 => assert_eq!(vec!("Pear", "Green"), row),
+                2 => assert_eq!(vec!("Banana", "Yellow"), row),
+                3 => assert_eq!(vec!("Orange", "Orange"), row),
+                _ => panic!("Unexpected iteration")
+            }
+        }
+    }
+
+    #[test]
+    fn for_test() {
+        let m = Matrix::from_vec(build_data());
+        let mut i = 0;
+        for row in m {
+            match i {
+                0 => assert_eq!(vec!("Apple", "Red"), row),
+                1 => assert_eq!(vec!("Pear", "Green"), row),
+                2 => assert_eq!(vec!("Banana", "Yellow"), row),
+                3 => assert_eq!(vec!("Orange", "Orange"), row),
+                _ => panic!("Unexpected iteration")
+            }
+
+            i += 1;
+        }
     }
 
     #[test]
@@ -152,5 +274,43 @@ mod tests {
         assert_eq!(Matrix::<&str>::new(), Matrix::from_vec(vec!()));
         assert!(Matrix::<&str>::new().is_empty());
         assert!(Matrix::<&str>::from_vec(vec!()).is_empty());
+    }
+
+    #[test]
+    fn push_row_test() {
+        let mut m = Matrix::from_vec(build_data());
+
+        m.push_row(vec!("Grape", "Blue"));
+
+        assert_eq!(
+        vec!(
+            vec!("Apple", "Red"),
+            vec!("Pear", "Green"),
+            vec!("Banana", "Yellow"),
+            vec!("Orange", "Orange"),
+            vec!("Grape", "Blue"),
+            ),
+        m.data
+        );
+        assert_eq!(5, m.rows);
+    }
+
+    #[test]
+    fn prepend_row_test() {
+        let mut m = Matrix::from_vec(build_data());
+
+        m.prepend_row(vec!("Grape", "Blue"));
+
+        assert_eq!(
+        vec!(
+            vec!("Grape", "Blue"),
+            vec!("Apple", "Red"),
+            vec!("Pear", "Green"),
+            vec!("Banana", "Yellow"),
+            vec!("Orange", "Orange"),
+            ),
+        m.data
+        );
+        assert_eq!(5, m.rows);
     }
 }
