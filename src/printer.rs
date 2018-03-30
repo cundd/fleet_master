aaa0ccbfcea6271c7de539e6ea9b5e20122cb265
Daniel Corn <info@cundd.net>
2018-03-30 14:03:32 +0200

    Enhance the error output

--- src/printer.rs
+++ src/printer.rs
@@ -1,6 +1,3 @@
-use std::io::stderr;
-use std::io::stdout;
-use std::io::Write;
 use ansi_term::Colour;
 use error::Error;
 use error::ErrorCollection;
@@ -11,14 +8,14 @@ impl Printer {
     /// Prints the result
     pub fn print_result(o: Result<String, Error>) {
         match o {
-            Ok(text) => { let _ = writeln!(&mut stdout(), "{}", text); }
+            Ok(text) => { println!("{}", text); }
             Err(e) => Self::print_error(e),
         }
     }
 
     /// Prints an error's message to STDERR painted in red
     pub fn print_error(e: Error) {
-        let _ = writeln!(&mut stderr(), "{}", Colour::Red.paint(format!("error: {}", e.message())));
+        eprintln!("{}", Colour::Red.paint(format!("error: {}", e.message())));
     }
 
     /// Prints an error if the result is not ok. The Ok part of the result is ignored
@@ -31,13 +28,36 @@ impl Printer {
     /// Prints all errors in the given collection
     pub fn print_error_collection(collection: ErrorCollection) {
         for (host, e) in collection {
-            let _ = writeln!(&mut stderr(), "{}", Colour::Red.paint(format!("error for host \"{}\": {}", host, e.message())));
+            eprintln!(
+                "{}",
+                Colour::Red.paint(format!(
+                    "Error for host \"{}\": {}",
+                    host,
+                    prepare_message(e.message())
+                ))
+            );
         }
     }
 
     /// Prints a custom message and the error's message to STDERR painted in red
     #[allow(unused)]
     pub fn print_message_and_error(message: &str, e: Error) {
-        let _ = writeln!(&mut stderr(), "{}", Colour::Red.paint(format!("error: {}: {}", message, e.message())));
+        eprintln!(
+            "{}",
+            Colour::Red.paint(format!(
+                "error: {}: {}",
+                message,
+                prepare_message(e.message())
+            ))
+        );
+    }
+}
+
+fn prepare_message(message: &str) -> String {
+    let single_line = message.replace("\n", " ");
+    if single_line.chars().count() > 120 {
+        format!("{}…", single_line.chars().take(120).collect::<String>())
+    } else {
+        single_line
     }
 }
